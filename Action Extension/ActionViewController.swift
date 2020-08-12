434936c35526d21f4fc1e497ff65de4703c58438
Cay Zhang <zcy20020811@gmail.com>
2020-08-12 16:16:59 +0800

    added action extension; added open url hack

---
+++ Action Extension/ActionViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ActionViewController.swift
+//  Action Extension
+//
+//  Created by Cay Zhang on 2020/8/12.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Get the item[s] we're handling from the extension context.
+        
+        // For example, look for an image and place it into an image view.
+        // Replace this with something appropriate for the type[s] your extension supports.
+//        var imageFound = false
+//        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
+//            for provider in item.attachments! {
+//                if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
+//                    // This is an image. We'll load it, then place it in our image view.
+//                    weak var weakImageView = self.imageView
+//                    provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil, completionHandler: { (imageURL, error) in
+//                        OperationQueue.main.addOperation {
+//                            if let strongImageView = weakImageView {
+//                                if let imageURL = imageURL as? URL {
+//                                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
+//                                }
+//                            }
+//                        }
+//                    })
+//
+//                    imageFound = true
+//                    break
+//                }
+//            }
+            
+//            if (imageFound) {
+//                // We only handle one image, so stop looking for more.
+//                break
+//            }
+//        }
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+    func open(url urlComponents: URLComponents) {
+        guard let url = urlComponents.url else {
+            assertionFailure("URL conversion failed.")
+            return
+        }
+        let selector = sel_registerName("openURL:")
+        var responder = self as UIResponder?
+        while let r = responder, !r.responds(to: selector) {
+            responder = r.next
+        }
+        _ = responder?.perform(selector, with: url)
+    }
+
+    @IBAction func tap(_ sender: UIButton) {
+        open(url: URLComponents(string: "https://www.google.com")!)
+    }
+    
+}
