18647d10787f4b7dc564b403136b2d11b3b709d6
cayZ <13341339+Cay-Zhang@users.noreply.github.com>
2021-02-06 14:00:56 +0800

    added html parsing tests

--- RSSBudTests/RSSBudTests.swift
+++ RSSBudTests/RSSBudTests.swift
@@ -32,6 +32,53 @@ class RSSBudTests: XCTestCase {
         )
     }
     
+    func _testDetection(url: URLComponents, html: String = "", feedCount: Int) {
+        let expectation = self.expectation(description: "Detect \(feedCount) feeds with the given url and html.")
+        
+        RSSHub.Radar.detecting(url: url, html: html)
+            .sink { completion in
+                if case let .failure(error) = completion {
+                    XCTFail(error.localizedDescription)
+                }
+                expectation.fulfill()
+            } receiveValue: { feeds in
+                XCTAssertEqual(feeds.count, feedCount, "Unexpected feed count.")
+            }.store(in: &self.cancelBag)
+        
+        wait(for: [expectation], timeout: 3.0)
+    }
+    
+    func _testProcess(_ name: String, url: URLComponents, feedCount: Int) {
+        let expectation = self.expectation(description: "Detect \(feedCount) feeds from \(name).")
+        
+        RSSHub.Radar.asyncExpandURLAndGetHTML(for: url)
+            .prepend((url: url, html: ""))
+            .flatMap { tuple in
+                RSSHub.Radar.detecting(url: tuple.url, html: tuple.html ?? "")
+            }.scan([]) {
+                $0.count < $1.count ? $1 : $0
+            }.replaceEmpty(with: [])
+            .last()
+            .sink { completion in
+                if case let .failure(error) = completion {
+                    XCTFail(error.localizedDescription)
+                }
+                expectation.fulfill()
+            } receiveValue: { feeds in
+                XCTAssertEqual(feeds.count, feedCount, "Unexpected feed count.")
+            }.store(in: &self.cancelBag)
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func _testProcess(_ name: String? = nil, urlString: String, feedCount: Int = 1) {
+        if let url = URLComponents(autoPercentEncoding: urlString) {
+            _testProcess(name ?? "Untitled", url: url, feedCount: feedCount)
+        } else {
+            XCTFail("URL conversion failed.")
+        }
+    }
+    
     func testBilibiliSpace() throws {
         measureMetrics([.wallClockTime], automaticallyStartMeasuring: false) {
             let expectation = XCTestExpectation(description: "Detect the 8 feeds from the url.")
@@ -115,6 +162,19 @@ class RSSBudTests: XCTestCase {
             }.store(in: &self.cancelBag)
         
         wait(for: [expectation1, expectation2], timeout: 6.0)
+    }
+    
+    func testHTMLParsing() {
+        _testProcess("谷歌相册", urlString: "https://photos.google.com/share/AF1QipN-3SZHWnuYatO_p13elqJZjhIXBUV_ySkStFuYPXCusNA1U35Nwq5xeWqxEIfRRw?key=dzAzZGtzcUxpYW4wV2t6MXZJWk9VdURoUnJsSk1n", feedCount: 1)
+        _testProcess("Telegram 频道", urlString: "https://t.me/RSSBud", feedCount: 1)
+        _testProcess("Telegram 群组", urlString: "https://t.me/RSSBud_Discussion", feedCount: 0)
+        _testProcess("OneJAV BT 今日种子 & 今日演员", urlString: "https://onejav.com/", feedCount: 2)
+        _testProcess("OneJAV BT 页面种子", urlString: "https://onejav.com/search/IPX177", feedCount: 1)
+        _testProcess("语雀知识库", urlString: "https://www.yuque.com/pocv40/alcg2a", feedCount: 1)
+        _testProcess("即刻用户动态 (转发页面)", urlString: "https://m.okjike.com/reposts/5ef6a99228bd5e0018a94fd1", feedCount: 1)
+        //        _testProcess("微博博主 (昵称)", urlString: "https://weibo.com/hu_ge", feedCount: 1)
+        //        _testProcess("快递 100 快递追踪", urlString: "https://kuaidi100.com/", feedCount: 1)
+        //        _testProcess("Behance User", urlString: "https://www.behance.net/mishapetrick", feedCount: 1)
     }
     
     func testRSSHubRadarRulesConsistency() {
