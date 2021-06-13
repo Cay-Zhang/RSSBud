2944f2b9197c0c2e782bea2913f72f7459062c11
cayZ <13341339+Cay-Zhang@users.noreply.github.com>
2021-06-13 15:37:04 +0800

    Combine+Async: added AsyncFuture

--- Shared/Extensions/Combine+Async.swift
+++ Shared/Extensions/Combine+Async.swift
@@ -34,3 +34,44 @@ extension Publisher {
         }
     }
 }
+
+final class AsyncFuture<Output>: Publisher {
+    
+    typealias Failure = Error
+    
+    let priority: Task.Priority?
+    let task: @Sendable () async throws -> Output
+    
+    init(priority: Task.Priority? = nil, task: @escaping @Sendable () async throws -> Output) {
+        self.priority = priority
+        self.task = task
+    }
+    
+    func receive<S>(subscriber: S) where S: Subscriber, Failure == S.Failure, Output == S.Input {
+        subscriber.receive(subscription: Subscription(subscriber: subscriber, priority: priority, task: task))
+    }
+    
+    private class Subscription<Subscriber: Combine.Subscriber>: Combine.Subscription where Subscriber.Input == Output, Subscriber.Failure == Error {
+        var taskHandle: Task.Handle<Void, Never>?
+        
+        init(subscriber: Subscriber, priority: Task.Priority? = nil, task: @escaping @Sendable () async throws -> Output) {
+            self.taskHandle = async(priority: priority) {
+                do {
+                    if Task.isCancelled { return }
+                    let result = try await task()
+                    _ = subscriber.receive(result)
+                    subscriber.receive(completion: .finished)
+                } catch {
+                    subscriber.receive(completion: .failure(error))
+                }
+            }
+        }
+        
+        func request(_ demand: Subscribers.Demand) { }
+        
+        func cancel() {
+            taskHandle?.cancel()
+            taskHandle = nil
+        }
+    }
+}
