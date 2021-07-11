5769dcb8180f2e322b46289284246178c76e4d2b
cayZ <13341339+Cay-Zhang@users.noreply.github.com>
2021-07-11 18:50:34 +0800

    added BarProgressViewStyle

--- Shared/Extensions/Extensions.swift
+++ Shared/Extensions/Extensions.swift
@@ -324,3 +324,11 @@ extension LPLinkMetadata {
         }
     }
 }
+
+struct BarProgressViewStyle: ProgressViewStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        let progress = configuration.fractionCompleted ?? 0.5
+        return Color.accentColor
+            .scaleEffect(x: progress, y: 1, anchor: .leading)
+    }
+}
