7ac5a64d95b07302c644dc6dffc0f2c9764ec528
cayZ <13341339+Cay-Zhang@users.noreply.github.com>
2021-07-16 21:49:25 +0800

    added CayButtonStyle and WideButtonContainerModifier

--- Shared/Extensions/WideButton.swift
+++ Shared/Extensions/WideButton.swift
@@ -15,6 +15,55 @@ struct SquashableButtonStyle: ButtonStyle {
     }
 }
 
+struct CayButtonStyle<ContainerModifier: ViewModifier>: PrimitiveButtonStyle {
+    var containerModifier: ContainerModifier
+    
+    @State private var isPressed: Bool = false
+    
+    init(containerModifier: ContainerModifier) {
+        self.containerModifier = containerModifier
+    }
+    
+    init(wideContainerWithBackgroundColor backgroundColor: Color) where ContainerModifier == WideButtonContainerModifier {
+        self.init(containerModifier: WideButtonContainerModifier(backgroundColor: backgroundColor))
+    }
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .compositingGroup()
+            .opacity(isPressed ? 0.5 : 1)
+            .modifier(containerModifier)
+            ._onButtonGesture { newValue in
+                withAnimation(.easeOut(duration: newValue ? 0.1 : 0.4)) {
+                    isPressed = newValue
+                }
+                if newValue {
+                    UIImpactFeedbackGenerator.light.impactOccurred(intensity: 0.5)
+                }
+            } perform: {
+                configuration.trigger()
+            }.scaleEffect(isPressed ? 0.97 : 1)
+    }
+}
+
+extension UIImpactFeedbackGenerator {
+    static let light: UIImpactFeedbackGenerator = UIImpactFeedbackGenerator(style: .light)
+}
+
+struct WideButtonContainerModifier: ViewModifier {
+    var backgroundColor: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .font(Font.body.weight(.semibold))
+            .foregroundStyle(.tint)
+            .padding(.vertical, 10)
+            .frame(maxWidth: .infinity)
+            .background(backgroundColor)
+            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+    }
+}
+
 extension View {
     func roundedRectangleBackground(color uiColor: UIColor = .tertiarySystemBackground) -> some View {
         self.font(Font.body.weight(.semibold))
