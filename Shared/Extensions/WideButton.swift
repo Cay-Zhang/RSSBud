c2aae73163a4e1620fe6ab83e8c41e6febcbb4d6
cayZ <13341339+Cay-Zhang@users.noreply.github.com>
2021-07-16 21:14:54 +0800

    removed WideButton implementation

--- Shared/Extensions/WideButton.swift
+++ Shared/Extensions/WideButton.swift
@@ -7,47 +7,6 @@
 
 import SwiftUI
 
-struct WideButton: View {
-    
-    var action: () -> Void
-    var label: Label<Text, Image>
-    var backgroundColor: UIColor
-    
-    init(_ label: Label<Text, Image>, backgroundColor: UIColor = .tertiarySystemBackground, action: @escaping () -> Void) {
-        self.action = action
-        self.label = label
-        self.backgroundColor = backgroundColor
-    }
-    
-    init(_ label: Label<Text, Image>, backgroundColor: UIColor = .tertiarySystemBackground, withAnimation animation: Animation?, action: @escaping () -> Void) {
-        self.action = { withAnimation(animation, action) }
-        self.label = label
-        self.backgroundColor = backgroundColor
-    }
-    
-    var body: some View {
-        Button {
-            action()
-        } label: {
-            label.roundedRectangleBackground(color: backgroundColor)
-        }.buttonStyle(SquashableButtonStyle())
-    }
-}
-
-extension WideButton {
-    init(_ titleKey: LocalizedStringKey, systemImage iconName: String, backgroundColor: UIColor = .tertiarySystemBackground, action: @escaping () -> Void) {
-        self.action = action
-        self.label = Label(titleKey, systemImage: iconName)
-        self.backgroundColor = backgroundColor
-    }
-    
-    init(_ titleKey: LocalizedStringKey, systemImage iconName: String, backgroundColor: UIColor = .tertiarySystemBackground, withAnimation animation: Animation?, action: @escaping () -> Void) {
-        self.action = { withAnimation(animation, action) }
-        self.label = Label(titleKey, systemImage: iconName)
-        self.backgroundColor = backgroundColor
-    }
-}
-
 struct SquashableButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
