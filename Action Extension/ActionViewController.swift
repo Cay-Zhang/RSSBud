795326dd91c5537c2c2fbe064d2053cf7b9f7336
Cay Zhang <13341339+Cay-Zhang@users.noreply.github.com>
2023-07-24 19:59:34 -0500

    Increase preferred content size of action extension

--- Action Extension/ActionViewController.swift
+++ Action Extension/ActionViewController.swift
@@ -31,10 +31,9 @@ class ActionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // if host view cannot fit the preferred content size,
-        // a sheet will be used instead of a popover
-        // size class is not assigned properly
-        preferredContentSize = CGSize(width: 450, height: 900)
+        // If host view cannot fit the preferred content size, a sheet will be used instead of a popover.
+        // On Apple silicon macs, for some reason the horizontal size class given is always `.regular`.
+        preferredContentSize = CGSize(width: 900, height: 900)
         
         // temp workaround for list background
         UITableView.appearance().backgroundColor = UIColor.clear
